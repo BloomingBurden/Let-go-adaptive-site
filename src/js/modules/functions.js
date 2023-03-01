a3c072cbaf6765b76e3907e79120e4af5b0b35cb
Blooming Burden <flowersoverskulls96@gmail.com>
2023-03-01 15:45:35 +0300

    Ready scroll for header and start about section

--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -13,20 +13,65 @@ function isWebp() {
     });
 }
 
-const onClickBurger = () => {
+const onClickBurger = (evt) => {
+    const target = evt.target;
 
-    const toggleMenuBurger = (evt) => {
-        const target = evt.target;
+    if (!target.closest('.burger')) return;
 
-        if (!target.closest('.burger')) return;
+    const burgerMenu = document.querySelector('[data-burger]');
 
-        const burgerMenu = document.querySelector('[data-burger]');
-
-        burgerMenu.dataset.burger === 'opened' ? burgerMenu.dataset.burger = 'closed' : burgerMenu.dataset.burger = 'opened';
-    }
-
-    document.addEventListener('click', toggleMenuBurger);
+    burgerMenu.dataset.burger === 'opened' ? burgerMenu.dataset.burger = 'closed' : burgerMenu.dataset.burger = 'opened';
 };
 
+const debounce = (func, ms) => {
+    let timer = false;
+
+    return function (...arg) {
+        if (timer === true) {
+            return;
+        } 
+        timer = true;
+        func(arg);
+        
+        setTimeout(() => timer = false, ms);
+    }
+}
+
+const moveHeader = () => {
+    const header = document.querySelector('.header');
+    const headerImg = document.querySelector('.header__logo-top .logo__img');
+    const bodyImg = document.querySelector('.header__logo-body .logo__img');
+
+    const PATHS = {
+        phone: [`img/logo-black.png`,`img/logo-white.png`],
+        desktop: [`img/logo-tablet-black.png`,`img/logo-tablet.png`],
+    };
+
+    const setNewImg = (isScrolled) => {
+        if (window.innerWidth < 768) {
+            headerImg.src = isScrolled ? PATHS.phone[0] : PATHS.phone[1];
+        } else if (window.innerWidth >= 768 && window.innerWidth < 1440 ) {
+            headerImg.previousElementSibling.srcset = isScrolled ? PATHS.desktop[0] : PATHS.desktop[1];
+        } else {
+            bodyImg.previousElementSibling.previousElementSibling.srcset = isScrolled ? PATHS.desktop[0] : PATHS.desktop[1];
+        }
+    }
+
+    const onMoveHeader = (evt) => {
+        if (window.pageYOffset > 0) {
+            header.classList.add('header--scrolled');
+            
+            setNewImg(true);
+        } else {
+            header.classList.remove('header--scrolled');
+            setNewImg(false);
+        }
+    }
+
+    window.addEventListener('scroll', onMoveHeader);
+};
+
+
 isWebp();
-onClickBurger();
+moveHeader();
+document.addEventListener('click', onClickBurger);
