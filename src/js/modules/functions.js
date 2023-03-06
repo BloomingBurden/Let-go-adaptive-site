f4cd1dbb94f01d648b2983e1c4d791a838077d5c
Blooming Burden <flowersoverskulls96@gmail.com>
2023-03-06 16:23:14 +0300

    Ready companion, fix moveEffectstAnimation

--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -23,5 +23,17 @@ const onClickBurger = (evt) => {
     burgerMenu.dataset.burger === 'opened' ? burgerMenu.dataset.burger = 'closed' : burgerMenu.dataset.burger = 'opened';
 };
 
+const setPeopleLevel = () => {
+    const level = document.querySelectorAll('.people__level');
+
+    level.forEach(item => {
+        const svgCircle = item.querySelector('.people__circle-main');
+        const level = item.querySelector('b');
+
+        svgCircle.style.strokeDashoffset = Math.ceil(180 - (level.textContent * 1.76));
+    })
+}
+
 isWebp();
+setPeopleLevel();
 document.addEventListener('click', onClickBurger);
