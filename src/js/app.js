e654db12185d564445d675fb0c5980d234894595
Blooming Burden <flowersoverskulls96@gmail.com>
2023-03-08 11:26:07 +0300

    Finished project

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,6 @@
 import './modules/functions.js';
 import './modules/setMoveEffects.js';
+import './modules/yamaps.js';
 import { moveHeader } from './modules/moveHeader.js';
 import { setNavbarLevel } from './modules/navbarLevel.js';
 
